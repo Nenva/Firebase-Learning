b89242b648614c1f46b2073eb155aad623887d29
Moisés Gómez Medina <50937926+Nenva@users.noreply.github.com>
2020-10-10 08:13:22 -0700

    Google Authentication

--- app/src/main/java/mgm/u/firebase_learning/LoginActivity.kt
+++ app/src/main/java/mgm/u/firebase_learning/LoginActivity.kt
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -13,6 +17,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+
+const val REQUEST_CODE_SIGN_IN = 0
 
 class LoginActivity : AppCompatActivity() {
 
@@ -25,6 +31,17 @@ class LoginActivity : AppCompatActivity() {
 
         buttonLogin.setOnClickListener {
             loginUser()
+        }
+
+        buttonGoogleSignIn.setOnClickListener {
+            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.webclient_id))
+                .requestEmail()
+                .build()
+            val signInClient = GoogleSignIn.getClient(this, options)
+            signInClient.signInIntent.also {
+                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+            }
         }
 
         textRegisterActivity.setOnClickListener {
@@ -43,7 +60,6 @@ class LoginActivity : AppCompatActivity() {
                     auth.signInWithEmailAndPassword(email, password).await()
                     withContext(Dispatchers.Main) {
                         transitionToMainActivity()
-
                     }
                 } catch (e: Exception) {
                     withContext(Dispatchers.Main) {
@@ -57,6 +73,34 @@ class LoginActivity : AppCompatActivity() {
     private fun transitionToMainActivity() {
         Intent(this, MainActivity::class.java).also {
             startActivity(it)
+        }
+    }
+
+    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credentials).await()
+                transitionToMainActivity()
+                runOnUiThread {
+                    Toast.makeText(this@LoginActivity, "Successfully logged in!", Toast.LENGTH_LONG).show()
+                }
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
         }
     }
 }
